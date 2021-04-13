5caa51b0674149786bc8579483d50083347659b4
idesignerse <heberandrade@gmail.com>
2021-04-13 16:00:06 +0200

    adding EF
    Adding EF. Sql,, Tools

---
+++ TodoApi/VehicleContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi
+{
+    public class VehicleContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+            @"Data Source = S5D011\SQLEXPRESS; Initial Catalog = AssetsCatalogWA;" +
+            " Integrated Security = True; Connect Timeout = 30; Encrypt = False;" +
+            " TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"
+        );
+        }
+    }
+}
+
